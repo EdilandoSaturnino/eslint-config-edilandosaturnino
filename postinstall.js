cafda55acc961ad9fb581f881f07cbabd46fd42a
Edilando Saturnino <ediralando@gmail.com>
2024-01-18 20:45:18 -0300

    Add files via upload

--- postinstall.js
+++ postinstall.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const sourceFile = path.join(__dirname, '.eslintrc.cjs');
 
-const targetFile = path.join(__dirname, '..', '..', '.eslintrc.cjs');
+const targetFile = path.join(__dirname, '..', '..', '..', '.eslintrc.cjs');
 
 fs.copyFileSync(sourceFile, targetFile);
 
